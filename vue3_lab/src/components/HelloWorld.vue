16b60ccaf60276faa7fa4a67c116f01843736309
LiGuangQiao <lgqfighting@163.com>
2024-10-29 10:56:21 +0000

    wangeditor测试

---
+++ vue3_lab/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import axios from 'axios';
+declare global {
+  interface Window {
+    editor: {
+      getHtml: () => string;
+    };
+  }
+}
+// 定义 post 请求方法
+const sendPostRequest = async () => {
+  try {
+    const response = await axios.post('/office/inserthtml', {
+      html: window.editor.getHtml()
+    });
+    console.log('响应数据:', response.data);
+  } catch (error) {
+    console.error('请求失败:', error);
+  }
+};
+</script>
+
+<template>
+  <div class="card">
+    <button type="button" @click="sendPostRequest();">post</button>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
